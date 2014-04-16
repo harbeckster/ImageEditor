846276070b328dde3cd7d69de5cc3322d202e2d6
Felix <FeJa-92@gmx.de>
2014-04-16 14:25:35 +0200

    Added MenuBar

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -1,8 +1,27 @@
 import javax.swing.*;
+import java.awt.event.*;
 public class ImageEditorFrame extends JFrame{    
 	public ImageEditorFrame(){       
 		setDefaultCloseOperation(EXIT_ON_CLOSE);       
 		setSize(400,  300);       
 		setVisible(true);    
+		createMenuBar();
+	}
+	private void createMenuBar(){
+		JMenuBar menuBar = new JMenuBar();   
+		setJMenuBar(menuBar);   
+		JMenu menuFile = new JMenu("File");   
+		menuBar.add(menuFile);   
+		JMenuItem menuItemOpen = new JMenuItem("Open");   
+		menuFile.add(menuItemOpen);   
+		menuItemOpen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+			onOpen();          
+			}       
+		}); 
 	} 
+	
+	private void onOpen(){ 
+		JOptionPane.showMessageDialog(this, "Open Selected"); 
+	}		
 }
